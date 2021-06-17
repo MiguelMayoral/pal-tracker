38d3f465b2baf7680e3db70f3a4ddd7c8b4f3e29
MiguelMayoral <miguel.mayoralbustos@gmail.com>
2021-06-17 14:24:41 +0000

    end of Jdbc lab

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -13,7 +13,7 @@ public class PalTrackerApplication {
     }
 
     @Bean
-    TimeEntryRepository timeEntryRepository(){
-        return new InMemoryTimeEntryRepository();
+    TimeEntryRepository timeEntryRepository(DataSource dataSource){
+        return new JdbcTimeEntryRepository(dataSource);
     }
 }
